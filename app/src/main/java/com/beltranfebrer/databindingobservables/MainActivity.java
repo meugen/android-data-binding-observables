dba3fcd73c79d2d3d511d200feab8f71d65b48b2
Miquel Beltran <mike513@gmail.com>
2016-01-31 14:51:11 +0100

    Changed to an Observable ModelView

--- app/src/main/java/com/beltranfebrer/databindingobservables/MainActivity.java
+++ app/src/main/java/com/beltranfebrer/databindingobservables/MainActivity.java
@@ -1,7 +1,10 @@
 package com.beltranfebrer.databindingobservables;
 
 import android.annotation.TargetApi;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableBoolean;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -29,23 +32,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLightChanged(CompoundButton view, boolean isChecked) {
-        viewModel.isOn = isChecked;
-        binding.setViewModel(viewModel);
+        viewModel.isOn.set(isChecked);
     }
 
     public void onColorChanged(CompoundButton view, boolean isChecked) {
-        viewModel.isColor = isChecked;
-        binding.setViewModel(viewModel);
+        viewModel.isColor.set(isChecked);
     }
 
     public void onMessageChanged(CompoundButton view, boolean isChecked) {
-        viewModel.isChanged = isChecked;
-        binding.setViewModel(viewModel);
+        viewModel.isChanged.set(isChecked);
     }
 
-    public static class ViewModel {
-        public boolean isOn;
-        public boolean isColor;
-        public boolean isChanged;
+    public static class ViewModel extends BaseObservable {
+        @Bindable
+        public final ObservableBoolean isOn = new ObservableBoolean();
+        @Bindable
+        public final ObservableBoolean isColor = new ObservableBoolean();
+        @Bindable
+        public final ObservableBoolean isChanged = new ObservableBoolean();
     }
 }
